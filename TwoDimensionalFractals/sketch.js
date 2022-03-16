1dc652273fa32908ae7bae8bd381fae8ec2d5c0e
Pauwul's macbook <petrila.paulcosmin@gmail.com>
2022-03-16 22:29:40 +0200

    Add stop/play by pressing space

--- TwoDimensionalFractals/sketch.js
+++ TwoDimensionalFractals/sketch.js
@@ -1,13 +1,22 @@
-var rotat = 0;
 
+//let rotat = PI / 4;
+let rotat = 0;
+let growth = 0;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   slider = createSlider( 0, TWO_PI, PI/4, 0.01);
+  growth = random(0.5, 0.8);
 }
 
 function draw() {
   background(100);
-  rotat = slider.value();
+  if( key == " "){
+    
+    rotat = rotat + 0.03;
+    
+  }
+
+  // rotat = slider.value();
   stroke(255);
   translate(width/2, height);
   branch(height/4);
@@ -25,12 +34,12 @@ function branch(len)
     //push is used to save the current drawing state
     push();
     rotate( rotat );
-    branch(len*0.67);
+    branch(len*growth);
     pop();
     //pop us used to restore the stored drawing state
     push();
     rotate(-rotat);
-    branch(len * 0.67);
+    branch(len * growth);
     pop();
   }
 }
